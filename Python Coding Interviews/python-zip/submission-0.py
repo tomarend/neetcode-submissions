ab8e07184bd537d88a0978df9a18d1f6919d4c5a
Tom Arend <tom_arend@hotmail.de>
2026-07-13 09:48:26 +0200

    Add: python-zip - submission-0

---
+++ Python Coding Interviews/python-zip/submission-0.py
@@ -0,0 +1,13 @@
+from typing import List, Dict
+
+
+def group_names_and_scores(names: List[str], scores: List[int]) -> Dict[str, int]:
+    ns_dict = {}
+    for n, s in zip(names, scores):
+         ns_dict[n] = s
+    return ns_dict
+
+# do not modify below this line
+print(group_names_and_scores(["Alice", "Bob", "Charlie"], [90, 80, 70]))
+print(group_names_and_scores(["Jane", "Carol", "Charlie"], [25, 100, 60]))
+print(group_names_and_scores(["Doug", "Bob", "Tommy"], [80, 90, 100]))
